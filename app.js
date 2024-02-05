4fe6d4a797b7b4d1847786401131742a58a7bb02
Jessica Harper <jessicaharper@Jessicas-MacBook-Air.local>
2024-02-05 15:53:01 +0000

    fix cors issue

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const {
 } = require("./errors/index");
 const app = express();
 const apiRouter = require("./routes/api-router");
-app.use(express.json());
-
 app.use(cors());
+
+app.use(express.json());
 
 app.use("/api", apiRouter);
 
